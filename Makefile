03a2977ef03399367353028e388b756117997add
Isaac Brodsky <isaac@isaacbrodsky.com>
2024-06-25 11:10:21 -0500

    rename to h3

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 PROJ_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
 # Configuration of extension
-EXT_NAME=h3ext
+EXT_NAME=h3
 EXT_CONFIG=${PROJ_DIR}extension_config.cmake
 
 # Include the Makefile from extension-ci-tools
--- extension_config.cmake
+++ extension_config.cmake
@@ -1,7 +1,7 @@
 # This file is included by DuckDB's build system. It specifies which extension to load
 
 # Extension from this repo
-duckdb_extension_load(h3ext
+duckdb_extension_load(h3
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
 )
