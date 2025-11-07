26d43cf535099d78f171dc803b3d221fdffce53b
Isaac Brodsky <isaac@isaacbrodsky.com>
2025-11-06 17:40:41 -0800

    Prepare for 4.4.0 (#168)
    * wip
    
    * construct cell
    
    * format
    
    * update for h3 release

--- src/h3_inspection.cpp
+++ src/h3_inspection.cpp
@@ -56,6 +56,51 @@ static void GetBaseCellNumberVarcharFunction(DataChunk &args,
       });
 }
 
+template <typename T>
+static void GetIndexDigitFunction(DataChunk &args, ExpressionState &state,
+                                  Vector &result) {
+  auto &inputs = args.data[0];
+  auto &inputs2 = args.data[1];
+  BinaryExecutor::ExecuteWithNulls<T, int, int>(
+      inputs, inputs2, result, args.size(),
+      [&](T cell, int res, ValidityMask &mask, idx_t idx) {
+        int out;
+        H3Error err0 = getIndexDigit(cell, res, &out);
+        if (err0) {
+          mask.SetInvalid(idx);
+          return 0;
+        } else {
+          return out;
+        }
+      });
+}
+
+static void GetIndexDigitVarcharFunction(DataChunk &args,
+                                         ExpressionState &state,
+                                         Vector &result) {
+  auto &inputs = args.data[0];
+  auto &inputs2 = args.data[1];
+  BinaryExecutor::ExecuteWithNulls<string_t, int, int>(
+      inputs, inputs2, result, args.size(),
+      [&](string_t cellAddress, int res, ValidityMask &mask, idx_t idx) {
+        H3Index cell;
+        H3Error err0 = stringToH3(cellAddress.GetString().c_str(), &cell);
+        if (err0) {
+          mask.SetInvalid(idx);
+          return 0;
+        } else {
+          int out;
+          H3Error err1 = getIndexDigit(cell, res, &out);
+          if (err1) {
+            mask.SetInvalid(idx);
+            return 0;
+          } else {
+            return out;
+          }
+        }
+      });
+}
+
 static void StringToH3Function(DataChunk &args, ExpressionState &state,
                                Vector &result) {
   auto &inputs = args.data[0];
@@ -86,6 +131,29 @@ static void H3ToStringFunction(DataChunk &args, ExpressionState &state,
                                Vector &result) {
   UnaryExecutor::ExecuteString<T, string_t, H3ToStringOperator>(
       args.data[0], result, args.size());
+}
+
+static void IsValidIndexVarcharFunction(DataChunk &args, ExpressionState &state,
+                                        Vector &result) {
+  auto &inputs = args.data[0];
+  UnaryExecutor::Execute<string_t, bool>(
+      inputs, result, args.size(), [&](string_t input) {
+        H3Index h;
+        H3Error err = stringToH3(input.GetString().c_str(), &h);
+        if (err) {
+          return false;
+        }
+        return bool(isValidIndex(h));
+      });
+}
+
+template <typename T>
+static void IsValidIndexFunction(DataChunk &args, ExpressionState &state,
+                                 Vector &result) {
+  auto &inputs = args.data[0];
+  UnaryExecutor::Execute<T, bool>(inputs, result, args.size(), [&](T input) {
+    return bool(isValidIndex(input));
+  });
 }
 
 static void IsValidCellVarcharFunction(DataChunk &args, ExpressionState &state,
@@ -249,6 +317,188 @@ static void GetIcosahedronFacesVarcharFunction(DataChunk &args,
   result.Verify(args.size());
 }
 
+static void ConstructCellFunction(DataChunk &args, ExpressionState &state,
+                                  Vector &result) {
+  D_ASSERT(args.ColumnCount() == 3);
+  auto count = args.size();
+
+  Vector &resVec = args.data[0];
+  Vector &baseCellVec = args.data[1];
+  Vector &digitsVec = args.data[2];
+  if (resVec.GetType().id() == LogicalTypeId::SQLNULL) {
+    result.Reference(resVec);
+    return;
+  }
+  if (baseCellVec.GetType().id() == LogicalTypeId::SQLNULL) {
+    result.Reference(baseCellVec);
+    return;
+  }
+  if (digitsVec.GetType().id() == LogicalTypeId::SQLNULL) {
+    result.Reference(digitsVec);
+    return;
+  }
+
+  auto lists_size = ListVector::GetListSize(digitsVec);
+  auto &child_vector = ListVector::GetEntry(digitsVec);
+  child_vector.Flatten(lists_size);
+
+  UnifiedVectorFormat child_data;
+  child_vector.ToUnifiedFormat(lists_size, child_data);
+
+  UnifiedVectorFormat lists_data;
+  digitsVec.ToUnifiedFormat(count, lists_data);
+  auto list_entries = UnifiedVectorFormat::GetData<list_entry_t>(lists_data);
+
+  UnifiedVectorFormat res_data;
+  resVec.ToUnifiedFormat(count, res_data);
+
+  UnifiedVectorFormat base_cell_data;
+  baseCellVec.ToUnifiedFormat(count, base_cell_data);
+
+  result.SetVectorType(VectorType::FLAT_VECTOR);
+  auto result_entries = FlatVector::GetData<uint64_t>(result);
+  auto &result_validity = FlatVector::Validity(result);
+
+  idx_t offset = 0;
+  for (idx_t i = 0; i < count; i++) {
+    auto list_index = lists_data.sel->get_index(i);
+    if (!lists_data.validity.RowIsValid(list_index) ||
+        !res_data.validity.RowIsValid(i) ||
+        !base_cell_data.validity.RowIsValid(i)) {
+      result_validity.SetInvalid(i);
+      continue;
+    }
+
+    auto res =
+        resVec.GetValue(i).DefaultCastAs(LogicalType::INTEGER).GetValue<int>();
+    auto baseCell = baseCellVec.GetValue(i)
+                        .DefaultCastAs(LogicalType::INTEGER)
+                        .GetValue<int>();
+
+    if (list_entries[i].length != res) {
+      result_validity.SetInvalid(i);
+      continue;
+    }
+
+    vector<int> digits(list_entries[i].length);
+    for (size_t j = 0; j < list_entries[i].length; j++) {
+      if (child_data.validity.RowIsValid(
+              child_data.sel->get_index(list_entries[i].offset + j))) {
+        digits[j] =
+            ((int *)child_data
+                 .data)[child_data.sel->get_index(list_entries[i].offset + j)];
+      }
+    }
+
+    H3Index out;
+    H3Error err = constructCell(res, baseCell, digits.data(), &out);
+    if (err) {
+      result_validity.SetInvalid(i);
+    } else {
+      result.SetValue(i, Value::UBIGINT(out));
+    }
+  }
+
+  if (resVec.GetVectorType() == VectorType::CONSTANT_VECTOR &&
+      baseCellVec.GetVectorType() == VectorType::CONSTANT_VECTOR &&
+      digitsVec.GetVectorType() == VectorType::CONSTANT_VECTOR) {
+    result.SetVectorType(VectorType::CONSTANT_VECTOR);
+  }
+  result.Verify(args.size());
+}
+
+static void ConstructCellVarcharFunction(DataChunk &args,
+                                         ExpressionState &state,
+                                         Vector &result) {
+  D_ASSERT(args.ColumnCount() == 3);
+  auto count = args.size();
+
+  Vector &resVec = args.data[0];
+  Vector &baseCellVec = args.data[1];
+  Vector &digitsVec = args.data[2];
+  if (resVec.GetType().id() == LogicalTypeId::SQLNULL) {
+    result.Reference(resVec);
+    return;
+  }
+  if (baseCellVec.GetType().id() == LogicalTypeId::SQLNULL) {
+    result.Reference(baseCellVec);
+    return;
+  }
+  if (digitsVec.GetType().id() == LogicalTypeId::SQLNULL) {
+    result.Reference(digitsVec);
+    return;
+  }
+
+  auto lists_size = ListVector::GetListSize(digitsVec);
+  auto &child_vector = ListVector::GetEntry(digitsVec);
+  child_vector.Flatten(lists_size);
+
+  UnifiedVectorFormat child_data;
+  child_vector.ToUnifiedFormat(lists_size, child_data);
+
+  UnifiedVectorFormat lists_data;
+  digitsVec.ToUnifiedFormat(count, lists_data);
+  auto list_entries = UnifiedVectorFormat::GetData<list_entry_t>(lists_data);
+
+  UnifiedVectorFormat res_data;
+  resVec.ToUnifiedFormat(count, res_data);
+
+  UnifiedVectorFormat base_cell_data;
+  baseCellVec.ToUnifiedFormat(count, base_cell_data);
+
+  result.SetVectorType(VectorType::FLAT_VECTOR);
+  auto result_entries = FlatVector::GetData<string_t>(result);
+  auto &result_validity = FlatVector::Validity(result);
+
+  idx_t offset = 0;
+  for (idx_t i = 0; i < count; i++) {
+    auto list_index = lists_data.sel->get_index(i);
+    if (!lists_data.validity.RowIsValid(list_index) ||
+        !res_data.validity.RowIsValid(i) ||
+        !base_cell_data.validity.RowIsValid(i)) {
+      result_validity.SetInvalid(i);
+      continue;
+    }
+
+    auto res =
+        resVec.GetValue(i).DefaultCastAs(LogicalType::INTEGER).GetValue<int>();
+    auto baseCell = baseCellVec.GetValue(i)
+                        .DefaultCastAs(LogicalType::INTEGER)
+                        .GetValue<int>();
+
+    if (list_entries[i].length != res) {
+      result_validity.SetInvalid(i);
+      continue;
+    }
+
+    vector<int> digits(list_entries[i].length);
+    for (size_t j = 0; j < list_entries[i].length; j++) {
+      if (child_data.validity.RowIsValid(
+              child_data.sel->get_index(list_entries[i].offset + j))) {
+        digits[j] =
+            ((int *)child_data
+                 .data)[child_data.sel->get_index(list_entries[i].offset + j)];
+      }
+    }
+
+    H3Index out;
+    H3Error err = constructCell(res, baseCell, digits.data(), &out);
+    if (err) {
+      result_validity.SetInvalid(i);
+    } else {
+      auto str = StringUtil::Format("%llx", out);
+      result.SetValue(i, StringVector::AddString(result, str));
+    }
+  }
+
+  if (resVec.GetVectorType() == VectorType::CONSTANT_VECTOR &&
+      baseCellVec.GetVectorType() == VectorType::CONSTANT_VECTOR &&
+      digitsVec.GetVectorType() == VectorType::CONSTANT_VECTOR) {
+    result.SetVectorType(VectorType::CONSTANT_VECTOR);
+  }
+  result.Verify(args.size());
+}
+
 CreateScalarFunctionInfo H3Functions::GetGetResolutionFunction() {
   ScalarFunctionSet funcs("h3_get_resolution");
   funcs.AddFunction(ScalarFunction({LogicalType::UBIGINT}, LogicalType::INTEGER,
@@ -271,6 +521,20 @@ CreateScalarFunctionInfo H3Functions::GetGetBaseCellNumberFunction() {
   return CreateScalarFunctionInfo(funcs);
 }
 
+CreateScalarFunctionInfo H3Functions::GetGetIndexDigitFunction() {
+  ScalarFunctionSet funcs("h3_get_index_digit");
+  funcs.AddFunction(ScalarFunction({LogicalType::UBIGINT, LogicalType::INTEGER},
+                                   LogicalType::INTEGER,
+                                   GetIndexDigitFunction<uint64_t>));
+  funcs.AddFunction(ScalarFunction({LogicalType::BIGINT, LogicalType::INTEGER},
+                                   LogicalType::INTEGER,
+                                   GetIndexDigitFunction<int64_t>));
+  funcs.AddFunction(ScalarFunction({LogicalType::VARCHAR, LogicalType::INTEGER},
+                                   LogicalType::INTEGER,
+                                   GetIndexDigitVarcharFunction));
+  return CreateScalarFunctionInfo(funcs);
+}
+
 CreateScalarFunctionInfo H3Functions::GetStringToH3Function() {
   return CreateScalarFunctionInfo(
       ScalarFunction("h3_string_to_h3", {LogicalType::VARCHAR},
@@ -283,6 +547,17 @@ CreateScalarFunctionInfo H3Functions::GetH3ToStringFunction() {
                                    H3ToStringFunction<uint64_t>));
   funcs.AddFunction(ScalarFunction({LogicalType::BIGINT}, LogicalType::VARCHAR,
                                    H3ToStringFunction<int64_t>));
+  return CreateScalarFunctionInfo(funcs);
+}
+
+CreateScalarFunctionInfo H3Functions::GetIsValidIndexFunctions() {
+  ScalarFunctionSet funcs("h3_is_valid_index");
+  funcs.AddFunction(ScalarFunction({LogicalType::VARCHAR}, LogicalType::BOOLEAN,
+                                   IsValidIndexVarcharFunction));
+  funcs.AddFunction(ScalarFunction({LogicalType::UBIGINT}, LogicalType::BOOLEAN,
+                                   IsValidIndexFunction<uint64_t>));
+  funcs.AddFunction(ScalarFunction({LogicalType::BIGINT}, LogicalType::BOOLEAN,
+                                   IsValidIndexFunction<int64_t>));
   return CreateScalarFunctionInfo(funcs);
 }
 
@@ -331,6 +606,22 @@ CreateScalarFunctionInfo H3Functions::GetGetIcosahedronFacesFunction() {
                                    LogicalType::LIST(LogicalType::INTEGER),
                                    GetIcosahedronFacesVarcharFunction));
   return CreateScalarFunctionInfo(funcs);
+}
+
+CreateScalarFunctionInfo H3Functions::GetConstructCellFunction() {
+  return CreateScalarFunctionInfo(
+      ScalarFunction("h3_construct_cell",
+                     {LogicalType::INTEGER, LogicalType::INTEGER,
+                      LogicalType::LIST(LogicalType::INTEGER)},
+                     LogicalType::UBIGINT, ConstructCellFunction));
+}
+
+CreateScalarFunctionInfo H3Functions::GetConstructCellVarcharFunction() {
+  return CreateScalarFunctionInfo(
+      ScalarFunction("h3_construct_cell_string",
+                     {LogicalType::INTEGER, LogicalType::INTEGER,
+                      LogicalType::LIST(LogicalType::INTEGER)},
+                     LogicalType::VARCHAR, ConstructCellVarcharFunction));
 }
 
 } // namespace duckdb
--- src/include/h3_functions.hpp
+++ src/include/h3_functions.hpp
@@ -30,10 +30,14 @@ public:
     functions.push_back(GetGetBaseCellNumberFunction());
     functions.push_back(GetStringToH3Function());
     functions.push_back(GetH3ToStringFunction());
+    functions.push_back(GetIsValidIndexFunctions());
     functions.push_back(GetIsValidCellFunctions());
     functions.push_back(GetIsResClassIIIFunction());
     functions.push_back(GetIsPentagonFunction());
     functions.push_back(GetGetIcosahedronFacesFunction());
+    functions.push_back(GetGetIndexDigitFunction());
+    functions.push_back(GetConstructCellFunction());
+    functions.push_back(GetConstructCellVarcharFunction());
 
     // Hierarchy
     functions.push_back(GetCellToParentFunction());
@@ -111,10 +115,14 @@ private:
   static CreateScalarFunctionInfo GetGetBaseCellNumberFunction();
   static CreateScalarFunctionInfo GetStringToH3Function();
   static CreateScalarFunctionInfo GetH3ToStringFunction();
+  static CreateScalarFunctionInfo GetIsValidIndexFunctions();
   static CreateScalarFunctionInfo GetIsValidCellFunctions();
   static CreateScalarFunctionInfo GetIsResClassIIIFunction();
   static CreateScalarFunctionInfo GetIsPentagonFunction();
   static CreateScalarFunctionInfo GetGetIcosahedronFacesFunction();
+  static CreateScalarFunctionInfo GetGetIndexDigitFunction();
+  static CreateScalarFunctionInfo GetConstructCellFunction();
+  static CreateScalarFunctionInfo GetConstructCellVarcharFunction();
 
   // Hierarchy
   static CreateScalarFunctionInfo GetCellToParentFunction();
