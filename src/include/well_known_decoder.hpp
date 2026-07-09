70647bf5e5eefcf6eb9e2c6eda30a47621b37800
Isaac Brodsky <isaac@isaacbrodsky.com>
2026-07-09 10:22:51 -0700

    well known

--- src/include/well_known_decoder.hpp
+++ src/include/well_known_decoder.hpp
@@ -1,21 +1,21 @@
-//
-//#pragma once
-//
-//#include <h3api.h>
-//#include <string>
-//
-// namespace h3duckdb {
-//
-// void DecodeWkbPolygon(
-//    const std::string &input, GeoPolygon &polygon,
-//    duckdb::shared_ptr<std::vector<LatLng>> &outerVerts,
-//    std::vector<GeoLoop> &holes,
-//    std::vector<duckdb::shared_ptr<std::vector<LatLng>>> &holesVerts);
-//
-// void DecodeWktPolygon(
-//    const std::string &input, GeoPolygon &polygon,
-//    duckdb::shared_ptr<std::vector<LatLng>> &outerVerts,
-//    std::vector<GeoLoop> &holes,
-//    std::vector<duckdb::shared_ptr<std::vector<LatLng>>> &holesVerts);
-//
-//} // namespace h3duckdb
+
+#pragma once
+
+#include "h3_common.hpp"
+#include <vector>
+
+ namespace h3duckdb {
+
+ void DecodeWkbPolygon(
+    const std::string &input, GeoPolygon &polygon,
+    std::vector<LatLng> &outerVerts,
+    std::vector<GeoLoop> &holes,
+    std::vector<std::vector<LatLng>> &holesVerts);
+
+ void DecodeWktPolygon(
+    const std::string &input, GeoPolygon &polygon,
+    std::vector<LatLng> &outerVerts,
+    std::vector<GeoLoop> &holes,
+    std::vector<std::vector<LatLng>> &holesVerts);
+
+} // namespace h3duckdb
